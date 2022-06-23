52faebbe68f6e42a51dac6c85117151cc0988958
Mahesh Bansod <mahesh0bansod@gmail.com>
2022-06-23 15:05:43 +0530

    Added ability to add lists

--- app/new-list/new-list.js
+++ app/new-list/new-list.js
@@ -4,6 +4,16 @@ angular.module('myApp.new-list', [])
   .component('newList', {
     templateUrl: 'new-list/new-list.html',
     controller: 'NewListCtrl'
-  }).controller('NewListCtrl', [function() {
-
+  }).controller('NewListCtrl', ['ListsService', function(ListsService) {
+    let ctrl = this;
+    ctrl.list = {
+      title: "",
+    };
+    ctrl.addList = () => {
+      let list = ctrl.list;
+      ListsService.addList(list).then(id => {
+        ctrl.newListId = id;
+      })
+      list.title = "";
+    }
   }]);
--- app/services/list.service.js
+++ app/services/list.service.js
@@ -2,25 +2,49 @@
 (function() {
     angular.module('myApp')
     .service('ListsService', [function () {
-        let service = {};
-        service.getLists = () => {
-            return new Promise(r => r(MOCK_LISTS_DATA.lists.map(list => {
-                return {
-                    id: list.id,
-                    title: list.title,
-                }
-            })))
-        };
-        return service;
-    }])
 
-    const MOCK_LISTS_DATA = {
-        lists: [
-            {
-                id: "0",
-                title: "Unlisted",
-                list:[],
+        const MOCK_LISTS_DATA = {
+            lists: [
+                {
+                    id: "0",
+                    title: "Unlisted",
+                    list:[],
+                },
+            ]
+        };
+    
+        const list_data = [...MOCK_LISTS_DATA.lists];
+
+        let service = {
+            getLists: () => {
+                return new Promise(r => r(list_data.map(list => {
+                    return {
+                        id: list.id,
+                        title: list.title,
+                    }
+                })))
             },
-        ]
-    };
+            /**
+             * Return a promise which resolves to the id of the newly created list
+             * @param {new list} list 
+             * @returns 
+             */
+            addList: (list) => {
+                return new Promise((resolve, _reject) => {
+                    let id = newId();
+                    list_data.push({
+                        id,
+                        title: list.title,
+                        list: [],
+                    });
+                    resolve(id);
+                });
+            }
+        };
+
+        function newId() {
+            return Math.floor(Math.random()*1000).toString();
+        }
+        return service;
+    }]);
 })();
