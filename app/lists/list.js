f3097accce9a74a9bd887109dde48a71b10cefd0
Mahesh Bansod <mahesh0bansod@gmail.com>
2022-06-24 20:29:38 +0530

    Added list items and ability to add and delete and edit list items

--- app/lists/list.js
+++ app/lists/list.js
@@ -5,6 +5,29 @@ angular.module('myApp.list', [])
     templateUrl: 'lists/list.html',
     controller: 'ListCtrl',
     bindings: { list: '<' },
-  }).controller('ListCtrl', [function() {
-    
+  }).controller('ListCtrl', ['$scope', 'ListsService', function($scope, ListsService) {
+    this.editingTask = null; // holds the task that is currently being edited
+    this.addNewTask = () => {
+      ListsService.addNewTask(this.list).then(newItem => {
+        this.editingTask = newItem;
+        $scope.$apply();
+      });
+    };
+    this.setEdit = (list_item) => {
+      this.editingTask = list_item;
+    };
+    this.saveTask = (list_item) => {
+      ListsService.saveTask(list_item).then(res => {
+        if(res.success) {
+          this.editingTask = null;
+          $scope.$apply();
+        }
+      })
+    };
+    this.deleteTask = (list_item) => {
+      ListsService.deleteTask(list_item, this.list).then(_ => {
+        this.editingTask = null;
+        $scope.$apply();
+      })
+    };
   }]);
--- app/services/list.service.js
+++ app/services/list.service.js
@@ -12,6 +12,12 @@
                 },
             ]
         };
+
+        const DEFAULT_TASK_TITLES = [
+            "Pet your cat",
+            "Worry about your future",
+            "Buy groceries",
+        ];
     
         const list_data = [...MOCK_LISTS_DATA.lists];
 
@@ -44,11 +50,42 @@
                 return new Promise((resolve, _reject) => {
                     resolve(list_data.find(list => list.id === listId));
                 })
+            },
+            addNewTask: (list) => {
+                return new Promise((resolve, _reject)=> {
+                    let id = newId();
+                    let newItem = {
+                        id,
+                        title: getDefaultTaskTitle(),
+                        state: 'initial',
+                    };
+                    if(!list) {
+                        // add to unlisted if no list assigned - makes me think I should've went with attribs as list of list instead.
+                        let unlisted = list_data.find(list => list.id === "0");
+                        unlisted.list.push(newItem);
+                    } else {
+                        list.list.push(newItem);
+                    }
+                    resolve(newItem);
+                })
+            },
+            saveTask: (list_item) => {
+                return new Promise(r => r({success: true}));
+            },
+            deleteTask: (list_item, list) => {
+                return new Promise((r)=>{
+                    list.list = list.list.filter(item => item.id !== list_item.id);
+                    r(true);
+                });
             }
         };
 
         function newId() {
-            return Math.floor(Math.random()*1000).toString();
+            return Math.floor(Math.random()*100000).toString();
+        }
+
+        function getDefaultTaskTitle() {
+            return DEFAULT_TASK_TITLES[Math.floor(Math.random()*DEFAULT_TASK_TITLES.length)];
         }
         return service;
     }]);
