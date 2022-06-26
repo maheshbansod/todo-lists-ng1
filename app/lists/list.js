4bfb529b9059fa5b4ee58875ca1a2ca394d92b56
Mahesh Bansod <mahesh0bansod@gmail.com>
2022-06-26 16:55:03 +0530

    Added filtering

--- app/lists/list.js
+++ app/lists/list.js
@@ -7,6 +7,17 @@ angular.module('myApp.list', [])
     bindings: { list: '<' },
   }).controller('ListCtrl', ['$scope', 'ListsService', function($scope, ListsService) {
     this.editingTask = null; // holds the task that is currently being edited
+    this.currentFilter = 'all';
+    this.filter = (task, index, array) => {
+      if(this.currentFilter === 'all') {
+        return true;
+      }
+      let eq = this.currentFilter === 'done';
+      if(task.is_done === eq)
+        return true;
+      return false;
+    };
+    this.setItemFilter = (f) => this.currentFilter = f;
     this.addNewTask = () => {
       ListsService.addNewTask(this.list).then(newItem => {
         this.editingTask = newItem;
